f38093abb6b1b2aeb6b22b366453a963fc8a2733
davidmelendez <Dmelz11@github.com>
2023-10-17 07:05:42 -0700

    working on answerButtons and css

---
+++ script.js
@@ -0,0 +1,13 @@
+var timer;
+var questions=[
+    {
+        question:"Commonly used data types DO Not Include:",
+        {text:"1. strings", correct: false},
+        {text:"2. booleans", correct: false},
+        {text:"3. alerts", correct: true},
+        {text:"4. numbers", correct: false},
+    },
+    {
+        question:""
+    }
+]
